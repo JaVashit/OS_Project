08429363ec1226e891e9f4fd30e3abf624022858
LG\엘지 <zety201@gmail.com>
2016-11-08 18:30:30 +0900

    erase hell mode

--- fightingGame/game.hpp
+++ fightingGame/game.hpp
@@ -6,14 +6,14 @@ class Game
 public:
     Game(sf::RenderWindow* window);
     int Run();
-    void Hell();
+    
     int setScore(bool,int);
     int getScore(bool);
     
 private:
     int score1;
     int score2;
-    bool hell;
+   
     float ay, damage, dmgred;
     sf::RenderWindow * _myWindow;
 };
